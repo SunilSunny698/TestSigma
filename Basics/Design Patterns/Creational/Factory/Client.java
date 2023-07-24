d6f43ca6cfd9c4e03f06d901583535ef71747fc0
SunilSunny698 <104634803+SunilSunny698@users.noreply.github.com>
2023-07-24 10:44:46 +0530

    Update Client.java by removing some code

--- Basics/Design Patterns/Creational/Factory/Client.java
+++ Basics/Design Patterns/Creational/Factory/Client.java
@@ -1,25 +1,15 @@
 import java.util.Scanner;
 
-import Creator.AsusManufacturer;
 import Creator.Factory;
-import Creator.MsiManufacturer;
 
 public class Client {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("GPU, you wanna work with.. :");
         String user = in.next();
-        Factory f;
-        if(user.toLowerCase().equals("asus")){
-            f=new AsusManufacturer();
-            f.execute();
-        }
-        else if(user.toLowerCase().equals("msi")){
-            f=new MsiManufacturer();
-            f.execute();
-        }
-        else{
-            System.out.println("Not avaiable");
-        }
+        Factory f = Factory.configure(user);
+        f.execute();
+        
     }
 }
+
